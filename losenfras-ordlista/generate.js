43c21e921d2e869f6c94e40e9e5b4184baedaddc
Staffan Rydergård <sr@approach.se>
2017-11-28 10:16:01 +0100

    Fixed tabs/spaces in generator. Added a few tests for arguments

--- losenfras-ordlista/generate.js
+++ losenfras-ordlista/generate.js
@@ -3,30 +3,31 @@
 let fs = require('fs');
 
 let lineReader = require('readline').createInterface({
-    input: fs.createReadStream('./ord.runeberg.txt','utf8')
+	input: fs.createReadStream('./ord.runeberg.txt', 'utf8')
 });
-  
+
 let words = [];
 
 function include(word) {
-    let re = /^([a-zåäö]){3,10}$/;
-    return re.exec(word);
+	let re = /^([a-zåäö]){3,10}$/;
+	return re.exec(word);
 }
 
 lineReader.on('line', function (line) {
-    if (include(line))
-        words.push(line);
-    else
-        console.log('Omitted ' + line);
+	if (include(line)) {
+		words.push(line);
+	} else {
+		console.log('Omitted ' + line);
+	}
 });
 
-lineReader.on('close', function(){
-    let outputFilename = '../ord.json';
-    fs.writeFile(outputFilename, JSON.stringify(words), function(err) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("Dictionary saved. Number of words: " + words.length);
-        }
-    });
+lineReader.on('close', function () {
+	let outputFilename = '../ord.json';
+	fs.writeFile(outputFilename, JSON.stringify(words), function (err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Dictionary saved. Number of words: ' + words.length);
+		}
+	});
 });
--- test.js
+++ test.js
@@ -1,11 +1,26 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
-test(async t => {
+test('No args gives four words', async t => {
 	const alfy = alfyTest();
-	const result = await alfy();
+	const result = await alfy('');
 
 	t.true(result[0].title === result[0].arg);
 	t.true(result[0].arg.length >= 15);
 	t.true(result[0].arg.split(' ').length === 4);
+});
+
+test('First arg set number of words', async t => {
+	const numberOfWords = 6;
+	const alfy = alfyTest();
+	const result = await alfy(numberOfWords);
+	t.true(result[0].arg.split(' ').length === numberOfWords);
+});
+
+test('Second arg set separator', async t => {
+	const numberOfWords = 5;
+	const separator = '#';
+	const alfy = alfyTest();
+	const result = await alfy(numberOfWords + ' ' + separator);
+	t.true(result[0].arg.split(separator).length === numberOfWords);
 });
